9b4981c8b0f9d09129a069f9bc294b7cd76cc04d
siyi <siyi@mit.edu>
2024-03-12 17:18:46 -0400

    refactor compute_fid_scores.py interface for convenience

--- scripts/compute_fid_scores.py
+++ scripts/compute_fid_scores.py
@@ -10,15 +10,9 @@
 """
 import argparse
 import os
-import sys
-
 import torch
-
-import numpy as np
 from PIL import Image
-
 from cleanfid import fid
-
 import shutil
 
 from scene_synthesis.datasets.splits_builder import CSVSplitsBuilder
@@ -26,14 +20,20 @@ from scene_synthesis.datasets.threed_front import CachedThreedFront
 
 
 class ThreedFrontRenderDataset(object):
-    def __init__(self, dataset):
+    def __init__(self, dataset, 
+                 image_name="rendered_scene_notexture_256_nofloor.png"):
         self.dataset = dataset
+        self.image_name = image_name
+        print("Use {} as rendered scene image.".format(image_name))
 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, idx):
-        image_path = self.dataset[idx].image_path
+        image_path = os.path.join(
+            os.path.dirname(self.dataset[idx].image_path),
+            self.image_name
+        )
         img = Image.open(image_path)
         return img
 
@@ -52,16 +52,34 @@ def main(argv):
         help="Path to the folder containing the synthesized"
     )
     parser.add_argument(
-        "path_to_annotations",
-        help="Path to the folder containing the annotations"
+        "--path_to_annotations",
+        default=None,
+        help="Path to the file containing dataset splits"
+        "(default:../config/<room_type>_threed_front_splits.csv)"
     )
     parser.add_argument(
         "--compare_all",
         action="store_true",
         help="if compare all"
     )
+    parser.add_argument(
+        "--compare_test",
+        action="store_true",
+        help="if compare with the test split only"
+    )
 
     args = parser.parse_args(argv)
+    # Set default path_to_annotations if not specified
+    if args.path_to_annotations is None:
+        for room_type in ["bedroom", "diningroom", "livingroom"]:
+            default_path = f"../config/{room_type}_threed_front_splits.csv"
+            if room_type in args.path_to_real_renderings and os.path.exists(default_path):
+                args.path_to_annotations = default_path
+                break
+        else:
+            print("Cannot find dataset splits automatically. "
+                  "Please use '--path_to_annotations' to provide file path.")
+            return
 
     # Create Real datasets
     config = dict(
@@ -70,33 +88,25 @@ def main(argv):
     )
     splits_builder = CSVSplitsBuilder(args.path_to_annotations)
     if args.compare_all:
-        test_real = ThreedFrontRenderDataset(CachedThreedFront(
-            args.path_to_real_renderings,
-            config=config,
-            scene_ids=splits_builder.get_splits(["train", "val", "test"])
-        ))
+        scene_ids = splits_builder.get_splits(["train", "val", "test"])
+    elif args.compare_test:
+        scene_ids = splits_builder.get_splits(["test"])
     else:
-        test_real = ThreedFrontRenderDataset(CachedThreedFront(
-            args.path_to_real_renderings,
-            config=config,
-            scene_ids=splits_builder.get_splits(["train", "val"])
-        ))
+        scene_ids = splits_builder.get_splits(["train", "val"])
+    test_real = ThreedFrontRenderDataset(CachedThreedFront(
+        args.path_to_real_renderings, config=config, scene_ids=scene_ids
+    ))
 
-    print("Generating temporary a folder with test_real images...")
-    path_to_test_real = "/cluster/balrog/jtang/ATISS_exps/test_real/" # /tmp/test_real
-    if not os.path.exists(path_to_test_real):
-        os.makedirs(path_to_test_real)
+    path_to_test_real = "../output/fid_tmp/test_real/"
+    print("Generating a temporary folder with test_real images:", path_to_test_real)
+    os.makedirs(path_to_test_real)
     for i, di in enumerate(test_real):
         di.save("{}/{:05d}.png".format(path_to_test_real, i))
-    # Number of images to be copied
-    N = len(test_real)
     print('number of synthesized images :', len(test_real))
 
-    print("Generating temporary a folder with test_fake images...")
-    path_to_test_fake = "/cluster/balrog/jtang/ATISS_exps/test_fake/" #/tmp/test_fake/
-    if not os.path.exists(path_to_test_fake):
-        os.makedirs(path_to_test_fake)
-
+    path_to_test_fake = "../output/fid_tmp/test_fake/"
+    print("Generating a temporary folder with test_fake images:", path_to_test_fake)
+    os.makedirs(path_to_test_fake)
     synthesized_images = [
         os.path.join(args.path_to_synthesized_renderings, oi)
         for oi in os.listdir(args.path_to_synthesized_renderings)
@@ -104,8 +114,6 @@ def main(argv):
     ]
     print('number of synthesized images :', len(synthesized_images))
 
-    scores = []
-    scores2 = []
     for i, fi in enumerate(synthesized_images):
         shutil.copyfile(fi, "{}/{:05d}.png".format(path_to_test_fake, i))
 
@@ -114,6 +122,8 @@ def main(argv):
     print('fid score:', fid_score)
     kid_score = fid.compute_kid(path_to_test_real, path_to_test_fake, device=torch.device("cpu"))
     print('kid score:', kid_score)
+    
+    input("FID/KID score computed. Press any key to remove temporary image directories...")
     os.system('rm -r %s'%path_to_test_real)
     os.system('rm -r %s'%path_to_test_fake)
 
